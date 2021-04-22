236080dcee982f591b2d723b7047d350ad0aa429
tainakanchu <tainakanchu815@gmail.com>
2021-04-23 00:49:58 +0900

    ざっと作った

--- internet_sampler_flutter/internet_sampler_for_flutter/lib/main.dart
+++ internet_sampler_flutter/internet_sampler_for_flutter/lib/main.dart
@@ -1,24 +1,24 @@
+// import 'package:audioplayers/audioplayers_web.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  static FirebaseAnalytics analytics = FirebaseAnalytics(); //←追加
-
-  // This widget is the root of your application.
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Internet Sampler',
-      theme: ThemeData(
-        primarySwatch: Colors.yellow,
-      ),
       home: MyHomePage(title: 'Internet Sampler'),
-
       navigatorObservers: [
         FirebaseAnalyticsObserver(analytics: analytics),
       ],
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
   final String title;
 
   @override
@@ -35,39 +36,106 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildBody(),
     );
+  }
+
+  Widget _buildBody() {
+    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+    return FutureBuilder(
+      future: _initialization,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return _streamBuilder();
+        }
+        return LinearProgressIndicator();
+      },
+    );
+  }
+
+  Widget _streamBuilder() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('sampler').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return LinearProgressIndicator();
+        }
+        return _buildList(snapshot.data.docs);
+      },
+    );
+  }
+
+  Widget _buildList(List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) {
+        return _buildListItem(data);
+      }).toList(),
+    );
+  }
+
+  Widget _buildListItem(DocumentSnapshot data) {
+    final sampler = Sampler(data);
+
+    if (sampler.times > 0) {
+      sampler.play();
+    }
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+        child: ListTile(
+            title: Text(sampler.sampleType),
+            trailing: OutlinedButton(
+              onPressed: () {
+                sampler.reference.update({'times': 0});
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.delete,
+                  ),
+                  Text(sampler.times.toString()),
+                ],
+              ),
+            ),
+            onTap: () {
+              sampler.reference.update({'times': FieldValue.increment(1)});
+              // sampler.play();
+            }),
+      ),
+    );
+  }
+}
+
+class Sampler {
+  String sampleType;
+  int times;
+  DocumentReference reference;
+  AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
+
+  Sampler(DocumentSnapshot snapshot) {
+    var map = snapshot.data();
+    this.sampleType = map['type'];
+    this.times = map['times'];
+    this.reference = snapshot.reference;
+  }
+
+  void play() {
+    String filename = "$sampleType.wav";
+
+    final player = AudioCache();
+    player.play(filename);
   }
 }
