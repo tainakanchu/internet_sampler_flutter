0b4ccce8cd2f55324b18c30ff6a4a527495ea662
tainakanchu <tainakanchu815@gmail.com>
2021-06-06 16:55:45 +0900

    safearea

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appbar,
-      body: _buildBody(),
+      body: SafeArea(child: _buildBody()),
     );
   }
 
